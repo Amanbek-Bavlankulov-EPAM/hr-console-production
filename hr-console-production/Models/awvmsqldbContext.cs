5293dcdd46981e3e718f6a2f69c47dfc40d218a5
Amanbek Bavlankulov <Amanbek_Bavlankulov@epam.com>
2021-03-05 18:50:04 +0600

    test

--- hr-console-production/Models/awvmsqldbContext.cs
+++ hr-console-production/Models/awvmsqldbContext.cs
@@ -114,7 +114,6 @@ namespace hr_console_production.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Server=tcp:aw-vm-sql-server.database.windows.net,1433;Initial Catalog=aw-vm-sql-db;Persist Security Info=False;User ID=hradmin;Password=@!FA0mega;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
             }
         }

fdf82caf187b01852408f4717256d49e0080df45
Amanbek Bavlankulov <Amanbek_Bavlankulov@epam.com>
2021-03-12 04:55:13 +0600

    framework

--- hr-production-tests/ControllerTests.cs
+++ hr-production-tests/ControllerTests.cs
@@ -1,17 +1,19 @@
-using hr_console_production.Controllers;
+﻿using hr_console_production.Controllers;
 using Serilog;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace hr_production_tests
 {
-    public class UnitTest1
+    public class ControllerTests
     {
         [Fact]
         public void ThrowsException()
         {
-            var logger = new LoggerConfiguration()
-                .CreateLogger();
+            var logger = new LoggerConfiguration().CreateLogger();
             var controller = new ProductController(logger);
 
             Assert.Throws<NotImplementedException>(() => controller.GetException());
